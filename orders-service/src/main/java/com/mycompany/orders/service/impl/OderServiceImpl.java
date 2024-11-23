6efeff5f45af835f98f4f7d7075c49ee13179340
jvjara <jvjara@indracompany.com>
2024-11-23 13:59:08 -0500

    cambiar las llamadas a las apis anteriores por el puerto del gateway

--- orders-service/src/main/java/com/mycompany/orders/service/impl/OderServiceImpl.java
+++ orders-service/src/main/java/com/mycompany/orders/service/impl/OderServiceImpl.java
@@ -30,7 +30,7 @@ public class OderServiceImpl implements OrderService {
         // Check for inventory
         BaseResponse result = this.webClientBuilder.build()
                 .post()
-                .uri("http://localhost:8083/api/inventory/in-stock")
+                .uri("http://localhost:8080/api/inventory/in-stock")
                 .bodyValue(orderRequest.getOrderItems())
                 .retrieve()
                 .bodyToMono(BaseResponse.class)
